49d926a5484b0506524d412420a6178c10d9440b
linnamaa <anon@donotreply.fi>
2016-10-05 18:02:02 +0300

    updated update helper

--- update.sh
+++ update.sh
@@ -1,11 +1,14 @@
+#!/usr/bin/env bash
 #/bin/bash -xe
 
-ssh -i maankoodaus3.pem ec2-user@54.93.42.47  "kill $(ps aux | grep '[n]ode server' | awk '{print $2}')"
+IP=52.59.228.227
+# ssh -i maankoodaus3.pem ec2-user@54.93.42.47  "kill $(ps aux | grep '[n]ode server' | awk '{print $2}')"
 
-scp -i maankoodaus3.pem ./service/* ec2-user@54.93.42.47:/opt/mkk/
+scp -i maankoodaus3.pem ./* ec2-user@${IP}:/opt/mkk/
+scp -i maankoodaus3.pem ./service/* ec2-user@${IP}:/opt/mkk/service/
 
-scp -r -i maankoodaus3.pem ./service/config ec2-user@54.93.42.47:/opt/mkk/
-scp -r -i maankoodaus3.pem ./service/data ec2-user@54.93.42.47:/opt/mkk/
-scp -r -i maankoodaus3.pem ./service/handlers ec2-user@54.93.42.47:/opt/mkk/
+scp -r -i maankoodaus3.pem ./service/config ec2-user@${IP}:/opt/mkk/service/
+scp -r -i maankoodaus3.pem ./service/data ec2-user@${IP}:/opt/mkk/service/
+scp -r -i maankoodaus3.pem ./service/handlers ec2-user@${IP}:/opt/mkk/service/
 
-ssh -i maankoodaus3.pem ec2-user@54.93.42.47  "cd /opt/mkk | node server.js &"
+# ssh -i maankoodaus3.pem ec2-user@54.93.42.47  "cd /opt/mkk | node server.js &"
