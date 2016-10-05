83e2c46d536897f0c352310cc842037beb8d9edd
Antti Kettunen <antti.j.kettunen@tieto.com>
2016-10-05 12:40:45 +0300

    Added proper Icon format to .gitignore

--- service/server.js
+++ service/server.js
@@ -5,6 +5,7 @@ var Express = require('express');
 var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
 var Path = require('path');
+var mongoose = require('mongoose');
 
 var App = Express();
 
@@ -15,12 +16,14 @@ App.use(BodyParser.urlencoded({
     extended: true
 }));
 
+//mongoose.connect("mongodb://mongo:27017");
+
 App.use(Swaggerize({
     api: Path.resolve('./config/swagger.yaml'),
     handlers: Path.resolve('./handlers')
 }));
 
-Server.listen(8000, function () {
+Server.listen(3000, function () {
     App.swagger.api.host = this.address().address + ':' + this.address().port;
     /* eslint-disable no-console */
     console.log('App running on %s:%d', this.address().address, this.address().port);
