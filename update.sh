e702098f83f90237089d344f3ba5de2464f85c9d
linnamaa <anon@donotreply.fi>
2016-10-06 10:07:38 +0300

    updated update helper

--- update.sh
+++ update.sh
@@ -10,5 +10,6 @@ scp -i maankoodaus3.pem ./service/* ec2-user@${IP}:/opt/mkk/service/
 scp -r -i maankoodaus3.pem ./service/config ec2-user@${IP}:/opt/mkk/service/
 scp -r -i maankoodaus3.pem ./service/data ec2-user@${IP}:/opt/mkk/service/
 scp -r -i maankoodaus3.pem ./service/handlers ec2-user@${IP}:/opt/mkk/service/
+scp -r -i maankoodaus3.pem ./service/models ec2-user@${IP}:/opt/mkk/service/
 
 # ssh -i maankoodaus3.pem ec2-user@54.93.42.47  "cd /opt/mkk | node server.js &"
